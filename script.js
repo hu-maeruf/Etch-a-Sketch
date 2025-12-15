b923f7743720aab1366839c8892e721c797feb2d
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2025-12-15 09:50:59 +0300

    Add event listener to boxes to change background while hover

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ for (let i = 0; i < 256; i ++) {
     box.className = "box";
     containerBox.appendChild(box);
 }
+
+// Generate random color
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        box.style.background = `rgb(${red}, ${green}, ${blue})`;
+    });
+});
